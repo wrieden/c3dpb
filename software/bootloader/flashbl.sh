2345dab036be3cd9997acaa001e8806189cd2e6d
Sven Wrieden <wrieden@googlemail.com>
2020-03-15 23:42:35 +0100

    corrected openocd command

--- software/bootloader/flashbl.sh
+++ software/bootloader/flashbl.sh
@@ -7,12 +7,10 @@ make BOARD=c3dpb clean
 make BOARD=c3dpb
 
 flashfile=$(find . -regex '.+./bootloader-c3dpb.+.elf')
-openocd -f interface/stlink-v2.cfg -c "set CPUTAPID 0x2ba01477" -f target/at91samdXX.cfg -c "program $flashfile verify reset exit"
+openocd -f interface/stlink-v2.cfg -c "set CPUTAPID 0x2ba01477" -f target/atsame5x.cfg -c "adapter speed 100" -c "program $flashfile verify reset exit"
+
 
 make BOARD=c3dpb clean
 
 git apply --reverse ../c3dpb-bootloader.patch
 git stash pop
-
-cd ..
-
